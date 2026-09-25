91ae9b5be6a9ed70bc9e622737938f3c6f3dd02c
diego <deathgalvis@gmail.com>
2026-09-24 21:45:31 -0500

    was added alumnos component

---
+++ Frontend/src/app/componentes/alumnos/alumnos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-alumnos',
+  styleUrl: './alumnos.css',
+  templateUrl: './alumnos.html',
+})
+export class Alumnos {
+}
